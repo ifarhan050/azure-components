b66978fa687a1b8d7e4b739684119dd930ef3ffc
Muhammad Farhan Bin Irfan <35654899+ifarhan050@users.noreply.github.com>
2024-12-23 17:38:34 +0500

    Create api-task.cs

---
+++ tasks/api-task.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulumiGithub.Templates
+{
+    public class api_task
+    {
+        public void RetrieveData()
+        {
+            string task2 = "api task!";
+            Console.WriteLine(task2);
+
+        }
+    }
+}
