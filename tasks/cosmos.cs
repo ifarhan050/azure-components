8701e70a8162cc893854fd8a5e678ec35b57c303
Muhammad Farhan Bin Irfan <35654899+ifarhan050@users.noreply.github.com>
2024-12-23 17:41:11 +0500

    Create cosmos.cs

---
+++ tasks/cosmos.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulumiGithub.Templates
+{
+    internal class cosmos
+    {
+        public void RetrieveData()
+        {
+           Console.WriteLine("Hello cosmos");
+        }
+    }
+}
