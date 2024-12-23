a2895fb91250362f6942757ff38fa3971f425b08
Muhammad Farhan Bin Irfan <35654899+ifarhan050@users.noreply.github.com>
2024-12-23 17:37:45 +0500

    Create apim.cs

---
+++ tasks/apim.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulumiGithub.Templates
+{
+    public class apim
+    {
+        public void RetrieveData()
+        {
+            Console.WriteLine("Hello apim")
+
+        }
+    }
+}
