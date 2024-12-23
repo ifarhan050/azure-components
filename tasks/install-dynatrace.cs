2285a042f29aaf5468ae41fcd176f9b540bce057
Muhammad Farhan Bin Irfan <35654899+ifarhan050@users.noreply.github.com>
2024-12-23 17:39:37 +0500

    Create install-dynatrace.cs

---
+++ tasks/install-dynatrace.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulumiGithub.Templates
+{
+    internal class installdynatrace
+    {
+        public void RetrieveData()
+        {
+           Console.WriteLine("Hello installdynatrace");
+        }
+    }
+}
