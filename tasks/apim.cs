9503fab82e05d8b4c7f197c21d2319c8ee250de4
Muhammad Farhan Bin Irfan <35654899+ifarhan050@users.noreply.github.com>
2024-12-23 17:39:58 +0500

    Update apim.cs

--- tasks/apim.cs
+++ tasks/apim.cs
@@ -10,7 +10,7 @@ namespace PulumiGithub.Templates
     {
         public void RetrieveData()
         {
-            Console.WriteLine("Hello apim")
+            Console.WriteLine("Hello apim");
 
         }
     }
